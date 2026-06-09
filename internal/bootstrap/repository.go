b2912dfa81da8ec97486fcc5e10573c4d6338ead
15196735583 <1632743573@qq.com>
2026-06-09 12:58:55 +0800

    fix:补充game仓库

--- internal/bootstrap/repository.go
+++ internal/bootstrap/repository.go
@@ -3,19 +3,25 @@ package bootstrap
 import (
 	mysqlDB "galhub/internal/infrastructure/mysql"
 
+	mysqlGame "galhub/internal/infrastructure/mysql/game"
 	mysqlUser "galhub/internal/infrastructure/mysql/user"
 
+	domainGame "galhub/internal/domain/game"
 	domainUser "galhub/internal/domain/user"
 )
 
 type Repositories struct {
 	User domainUser.Repository
+	Game domainGame.Repository
 }
 
 func NewRepositories() *Repositories {
 
 	return &Repositories{
 		User: mysqlUser.NewRepository(
+			mysqlDB.DB,
+		),
+		Game: mysqlGame.NewRepository(
 			mysqlDB.DB,
 		),
 	}
